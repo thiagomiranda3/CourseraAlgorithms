db717e526c434f86fcd0a298c89ab7fd5a30136a
Thiago Miranda <marine.rlivielor@gmail.com>
2018-12-28 14:58:08 -0200

    versão otimizada do quickunion

---
+++ src/main/java/br/com/tommiranda/algorithms/QuickFindUF.java
@@ -0,0 +1,30 @@
+package br.com.tommiranda.algorithms;
+
+public class QuickFindUF {
+
+    private int[] id;
+
+    public QuickFindUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+
+    public int root(int i) {
+        while(i != id[i])
+            i = id[i];
+
+        return i;
+    }
+
+    public boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    public void union(int p, int q) {
+        int proot = root(p);
+        int qroot = root(q);
+
+        id[proot] = qroot;
+    }
+}
