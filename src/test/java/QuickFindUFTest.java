ca5bc47ed8e8fd21548dd38b60c2decc045865d8
Thiago Miranda <marine.rlivielor@gmail.com>
2019-01-07 08:32:15 -0200

    Adição de testes do QuickFind

--- src/test/java/QuickFindUFTest.java
+++ src/test/java/QuickFindUFTest.java
@@ -1,4 +1,5 @@
 import br.com.tommiranda.algorithms.QuickFindUF;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class QuickFindUFTest {
@@ -9,8 +10,18 @@ public class QuickFindUFTest {
 
         qf.union(0, 1);
         qf.union(5, 3);
+        qf.union(2, 6);
+        qf.union(6, 7);
         qf.union(3, 4);
+        qf.union(4, 6);
 
-        
+        Assertions.assertEquals(qf.connected(0, 1), true);
+        Assertions.assertEquals(qf.connected(5, 3), true);
+
+        // Tem que estar conectado indiretamente
+        Assertions.assertEquals(qf.connected(6, 5), true);
+
+        // Nenhuma conexão direta ou indireta
+        Assertions.assertEquals(qf.connected(0, 9), false);
     }
 }
