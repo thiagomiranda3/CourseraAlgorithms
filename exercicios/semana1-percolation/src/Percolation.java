92e80f570a4105ef4040462b6328d6ec2cc49e9b
Thiago Miranda <marine.rlivielor@gmail.com>
2019-08-20 02:37:18 +0000

    exercício semana 1

---
+++ exercicios/semana1-percolation/src/Percolation.java
@@ -0,0 +1,93 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+    private static final int IS_OPEN = 1;
+    private final WeightedQuickUnionUF wqu;
+    private int N;
+    private int openSites = 0;
+    private int indexTop;
+    private int indexBottom;
+    private int[][] matrix;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        this.N = n;
+        this.matrix = new int[N][N];
+        this.wqu = new WeightedQuickUnionUF((N * N) + 2);
+        this.indexTop = N * N;
+        this.indexBottom = (N * N) + 1;
+
+        for (int i = 0; i < N; i++) {
+            wqu.union(indexTop, getIndex(0, i));
+            wqu.union(indexBottom, getIndex(N - 1, i));
+        }
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        openSites++;
+        matrix[row][col] = IS_OPEN;
+
+        conectaVizinho(row, col, row - 1, col); // cima
+        conectaVizinho(row, col, row + 1, col); // baixo
+        conectaVizinho(row, col, row, col - 1); // esquerda
+        conectaVizinho(row, col, row, col + 1); // direita
+    }
+
+    private void conectaVizinho(int row, int col, int rowV, int colV) {
+        if (!checkRowCol(rowV, colV)) {
+            return;
+        }
+
+        if (matrix[rowV][colV] == IS_OPEN) {
+            wqu.union(getIndex(row, col), getIndex(rowV, colV));
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validateMatrix(row, col);
+
+        return matrix[row][col] == IS_OPEN;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validateMatrix(row, col);
+
+        return isOpen(row, col) && wqu.connected(indexTop, getIndex(row, col));
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return wqu.connected(indexTop, indexBottom);
+    }
+
+    private int getIndex(int row, int col) {
+        return col + (row * N);
+    }
+
+    private boolean checkRowCol(int row, int col) {
+        if (row < 0 || col < 0) {
+            return false;
+        }
+
+        return row < N && col < N;
+    }
+
+    private void validateMatrix(int row, int col) {
+        if (!checkRowCol(row, col)) {
+            throw new IllegalArgumentException("[" + row + ", " + col + "] is invalid");
+        }
+    }
+}
---
+++ exercicios/semana1-percolation/src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+    public static void main(String[] args) {
+        Percolation percolation = new Percolation(4);
+
+        percolation.open(0, 1);
+        percolation.open(1, 1);
+        percolation.open(1, 2);
+        percolation.open(2, 2);
+        percolation.open(2, 3);
+        percolation.open(3, 3);
+
+        System.out.println("Percolates: " + percolation.percolates());
+        System.out.println("Open Sites: " + percolation.numberOfOpenSites());
+    }
+}
